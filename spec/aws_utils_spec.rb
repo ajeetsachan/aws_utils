73097bacb61dc295dd3706e748d1e0ecdb0859ea
Anand Bagmar <abagmar@gmail.com>
2015-04-07 15:13:53 +0530

    added test to archive, upload and download from s3

--- spec/aws_utils_spec.rb
+++ spec/aws_utils_spec.rb
@@ -1,7 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "AwsUtils" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe 'Archive', :archive do
+  context "directories & files" do
+    it "should create zip file" do
+      Zip.archive_code
+    end
+  end
+end
+
+describe 'Archive and upload', :upload do
+  context "directories & files to S3" do
+    it "should create zip file and upload to S3" do
+      EC2.upload_to_s3(Zip.archive_code)
+      Utils.cleanup
+    end
+  end
+end
+
+describe 'Download', :download do
+  context "file from from S3" do
+    it "should download zip file from S3" do
+      EC2.get_from_s3
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,55 @@
-require 'simplecov'
-
-module SimpleCov::Configuration
-  def clean_filters
-    @filters = []
-  end
-end
-
-SimpleCov.configure do
-  clean_filters
-  load_adapter 'test_frameworks'
-end
-
-ENV["COVERAGE"] && SimpleCov.start do
-  add_filter "/.rvm/"
-end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'aws_utils'
+require 'pry'
+require 'rspec/expectations'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+def require_dependencies
+  `pwd`
+  ['lib'].each do |folder|
+    puts "foldeR: #{folder}"
+    Dir.glob(File.join("./", "#{folder}", "**", "*.rb")) do |file|
+      puts "file: #{file}"
+      require file
+    end
+  end
+end
+
+def before_each_spec(config)
+  config.before(:each) { |s|
+    puts "\nRunning spec: '#{s.example_group.metadata[:full_description]}'"
+    Utils.load_and_sanitize("input-data")
+    $validation_errors = []
+  }
+end
+
+def after_each_spec(config)
+  config.after(:each) {
+    if !$validation_errors.empty?
+      msg = "Spec failed. # of validation errors: #{$validation_errors.size}"
+      $validation_errors.each do |error|
+        msg += "\n\t" + error
+      end
+      $validation_errors.clear
+      raise StandardError.new("Boom! #{msg}")
+    end
+  }
+end
 
 RSpec.configure do |config|
-
+  config.run_all_when_everything_filtered = false
+  before_each_spec(config)
+  after_each_spec(config)
 end
+
+require_dependencies
+$time = Time.now
+$validation_errors=[]
+
+puts "Running tests at #{$time} against: #{$env}"
