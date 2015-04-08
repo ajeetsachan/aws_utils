f996e24a8553c31c8a42a358b481d02c94e5c249
Anand Bagmar <abagmar@gmail.com>
2015-04-08 10:25:23 +0530

    allow specifying input-data file from command line

--- spec/aws_utils_spec.rb
+++ spec/aws_utils_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe 'Archive', :archive do
   context "directories & files" do
     it "should create zip file" do
-      ec2 = EC2.new(File.join("spec", "resources", "input-data"))
+      input_file_name = get_input_file_name
+      ec2 = EC2.new(input_file_name)
       ec2.archive_code
     end
   end
@@ -12,7 +13,8 @@ end
 describe 'Archive and upload', :upload do
   context "directories & files to S3" do
     it "should create zip file and upload to S3" do
-      ec2 = EC2.new(File.join("spec", "resources", "input-data"))
+      input_file_name = get_input_file_name
+      ec2 = EC2.new(input_file_name)
       ec2.upload_to_s3_and_then_cleanup(ec2.archive_code)
     end
   end
@@ -21,8 +23,13 @@ end
 describe 'Download', :download do
   context "file from from S3" do
     it "should download zip file from S3" do
-      ec2 = EC2.new(File.join("spec", "resources", "input-data"))
+      input_file_name = get_input_file_name
+      ec2 = EC2.new(input_file_name)
       ec2.get_from_s3
     end
   end
+end
+
+def get_input_file_name
+  input_file_name = ENV['input_file_name'].nil? ? File.join("spec", "resources", "input-data") : ENV['input_file_name']
 end
